a7dc62fb49c8c585759a06aa408dfc04d5700014
ORTIZ <aidenortiz@mail.ru>
2023-03-04 21:46:47 +0300

    Version - Version 2.1.5 - Work on the Blog.jsx component was completed.
    js/jsx - The main content was added to the tags. The css module was imported. In tags through classes, CSS modules and their properties were added.
    
    css/scss - Basic properties of css styles, grid and flex elements were added through pseudoclasses, animations were added when interacting with content.

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,40 +2,59 @@ import img1 from "./image/img1.png";
 import img2 from "./image/img2.png";
 import img3 from "./image/img3.png";
 
+import styles from "./styles/Blog.module.css";
+
 function Blog() {
   return (
-    <section>
+    <section className={styles.blog}>
       <article>
-        <p></p>
-        <p></p>
+        <p className={styles.blog__title}>Our Latest Blog</p>
+        <p className={styles.blog__subtitle}>
+          It has survived not only five centuries, but also the
+        </p>
       </article>
-      <article>
-        <ul>
-          <li>
-            <img src="" alt="" />
-            <p></p>
-            <p></p>
-            <p></p>
-            <button></button>
+      <article className={styles.blog_news}>
+        <ul className={styles.blog_news__column}>
+          <li className={styles.blog_news__list}>
+            <img src={img1} alt="" />
+            <p className={styles.blog_news__data}>20 Agu 2022</p>
+            <p className={styles.blog_news__title}>
+              Mindfulness Activities for Kids & Toddlers with NFT
+            </p>
+            <p className={styles.blog_news__subtitle}>
+              In a free hour, when our power of choice is untrammelled and when
+              nothing prevents our being able to do what we like ...
+            </p>
+            <button className={styles.blog_news__button}>Read More</button>
           </li>
-          <li>
-            <img src="" alt="" />
-            <p></p>
-            <p></p>
-            <p></p>
-            <button></button>
+          <li className={styles.blog_news__list}>
+            <img src={img2} alt="" />
+            <p className={styles.blog_news__data}>20 Agu 2022</p>
+            <p className={styles.blog_news__title}>
+              Save Thousands Of Lives Through This NFT
+            </p>
+            <p className={styles.blog_news__subtitle}>
+              In a free hour, when our power of choice is untrammelled and when
+              nothing prevents our being able to do what we like ...
+            </p>
+            <button className={styles.blog_news__button}>Read More</button>
           </li>
-          <li>
-            <img src="" alt="" />
-            <p></p>
-            <p></p>
-            <p></p>
-            <button></button>
+          <li className={styles.blog_news__list}>
+            <img src={img3} alt="" />
+            <p className={styles.blog_news__data}>20 Agu 2022</p>
+            <p className={styles.blog_news__title}>
+              Mindfulness Activities for Kids & Toddlers with NFT
+            </p>
+            <p className={styles.blog_news__subtitle}>
+              In a free hour, when our power of choice is untrammelled and when
+              nothing prevents our being able to do what we like ...
+            </p>
+            <button className={styles.blog_news__button}>Read More</button>
           </li>
         </ul>
-        <div>
-          <span></span>
-          <span></span>
+        <div className={styles.blog_slider}>
+          <span className={styles.blog_slider__button}></span>
+          <span className={styles.blog_slider__button}></span>
         </div>
       </article>
     </section>
