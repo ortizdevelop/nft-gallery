2a34968edb597ebd072a7532d4597986cb6b0b38
ORTIZ <aidenortiz@mail.ru>
2023-03-04 10:44:34 +0300

    Version 2.1.0. Work has started on the Roadmap.jsx component.
    js/jsx - Created file Roadmap.jsx. In it were added the main tags to work.
    
    css/scss - Created the basic classes to work with component. Additional subclasses were added for them.
    
    img - images were added to work with the component.

---
+++ src/components/Roadmap/Roadmap.jsx
@@ -0,0 +1,92 @@
+import roadmap from "./image/Roadmap.png";
+import bg from "./image/bg.png";
+
+import styles from "./styles/Roadmap.module.css";
+
+const Roadmap = () => {
+  return (
+    <section>
+      <article>
+        <p></p>
+        <div>
+          <span></span>
+          <span></span>
+        </div>
+        <p></p>
+      </article>
+      <article>
+        <ul>
+          <li>
+            <p></p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipiscing elit. Natoque
+              viverra eget.
+            </p>
+            <a href="#s">
+              <span></span>
+            </a>
+          </li>
+          <li>
+            <p></p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipiscing elit. Natoque
+              viverra eget.
+            </p>
+            <a href="#s">
+              <span></span>
+            </a>
+          </li>
+          <li>
+            <p></p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipiscing elit. Natoque
+              viverra eget.
+            </p>
+            <a href="#s">
+              <span></span>
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <img src={roadmap} alt="" />
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p></p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipiscing elit. Natoque
+              viverra eget.
+            </p>
+            <a href="#s">
+              <span></span>
+            </a>
+          </li>
+          <li>
+            <p></p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipiscing elit. Natoque
+              viverra eget.
+            </p>
+            <a href="#s">
+              <span></span>
+            </a>
+          </li>
+          <li>
+            <p></p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipiscing elit. Natoque
+              viverra eget.
+            </p>
+            <a href="#s">
+              <span></span>
+            </a>
+          </li>
+        </ul>
+      </article>
+    </section>
+  );
+};
+
+export default Roadmap;
